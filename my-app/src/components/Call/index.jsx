be90d50bd4b92c79a694afa88e5d55690aa73c2b
Samuel Morais030202 <moraissamuel009@gmail.com>
2024-04-08 16:31:21 -0300

    feat: ajustando estilização da terceira seção

--- my-app/src/components/Call/index.jsx
+++ my-app/src/components/Call/index.jsx
@@ -1,31 +1,52 @@
+import style from './call.module.css';
+
 export default function Call() {
   return (
-    <section>
-      <h2>
+    <section className={style.callCard}>
+      <h2 className={style.callTitle}>
         Pra quem é a Sessão Estratégica
       </h2>
 
-      <p>
-        Você que não sabe colocar valor nos seus atendimentos 
-      </p>
+      <div className={style.cards}>
 
-      <p>
-        Você que tem uma carga horária de trabalho exaustiva 
-      </p>
+        <div className={style.card}>
+          <img src="/icon-verify.png" alt="icon" />
+          <p className={style.callText}>
+            Você que não sabe colocar valor nos seus atendimentos 
+          </p>
+        </div>
 
-      <p>
-        Você que dar desconto para seus pacientes porque tem
-        pena deles ficar sem tratamento
-      </p>
+        <div className={style.card}>
+          <img src="/icon-verify.png" alt="icon" />
+          <p className={style.callText}>
+            Você que tem uma carga horária de trabalho exaustiva 
+          </p>
+        </div>
 
-      <p>
-        Você que trabalha demais e no final não tem tempo para
-        fazer a gestão administrativa e hoje está tudo um “CAOS”
-      </p>
+        <div className={style.card}>
+          <img src="/icon-verify.png" alt="icon" />
+          <p className={style.callText}>
+            Você que dar desconto para seus pacientes porque tem
+            pena deles ficar sem tratamento
+          </p>
+        </div>
 
-      <p>
-        Você que ama o que faz mas está cansada e não sabe como se organizar 
-      </p>
+        <div className={style.card}>
+          <img src="/icon-verify.png" alt="icon" />
+          <p className={style.callText}>
+            Você que trabalha demais e no final não tem tempo para
+            fazer a gestão administrativa e hoje está tudo um “CAOS”
+          </p>
+        </div>
+
+        <div className={style.card}>
+          <img src="/icon-verify.png" alt="icon" />
+          <p className={style.callText}>
+            Você que ama o que faz mas está cansada e não sabe como se organizar 
+          </p>
+        </div>
+      </div>
+
     </section>
   )
 }
