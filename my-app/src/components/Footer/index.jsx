889e8ac277758d117978e3c3a15327816593b7c7
Samuel Morais030202 <moraissamuel009@gmail.com>
2024-04-08 17:35:53 -0300

    feat: criando footer mobile

--- my-app/src/components/Footer/index.jsx
+++ my-app/src/components/Footer/index.jsx
@@ -1,24 +1,29 @@
+import style from './footer.module.css';
+
 export default function Footer() {
   return (
-    <footer>
-      <p>
-        Este site NÃO é do Facebook:. Este site não faz parte do site do Facebook ou do
-        Facebook Inc. Além disso, este site NÃO é endossado pelo Facebook de nenhuma maneira.
-        FACEBOOK é uma marca comercial independente da FACEBOOK, Inc.
-      </p>
+    <footer className={style.footerCard}>
 
-      <p>
-        Nós NÃO compartilharemos seu endereço de e-mail com ninguém. Você pode cancelar sua
-        inscrição a qualquer momento. Ao se cadastrar você está aceitando receber comunicações
-        informativas e promocionais.
-      </p>
+      <div className={style.footerContainerText}>
+        <p className={style.footerText}>
+          Este site NÃO é do Facebook:. Este site não faz parte do site do Facebook ou do
+          Facebook Inc. Além disso, este site NÃO é endossado pelo Facebook de nenhuma maneira.
+          FACEBOOK é uma marca comercial independente da FACEBOOK, Inc.
+        </p>
 
-      <div>
-        
-        <p>Política de Privacidade  |  Termos de Uso</p>
+        <p className={style.footerText}>
+          Nós NÃO compartilharemos seu endereço de e-mail com ninguém. Você pode cancelar sua
+          inscrição a qualquer momento. Ao se cadastrar você está aceitando receber comunicações
+          informativas e promocionais.
+        </p>
 
-        <p>Copyright © Clínica Lara Oliveira  2024 - Todos direitos reservados.</p>
+        <div className={style.footerAlert}>
+          
+          <p>Política de Privacidade  |  Termos de Uso</p>
 
+          <p>Copyright © Clínica Lara Oliveira  2024 - Todos direitos reservados.</p>
+
+        </div>
       </div>
     </footer>
   )
