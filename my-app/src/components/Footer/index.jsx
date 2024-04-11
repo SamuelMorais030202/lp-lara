0a9d05ccd05ccdcd69f00f24b2d1510c1696b522
Samuel Morais030202 <moraissamuel009@gmail.com>
2024-04-11 01:15:16 -0300

    feat: ajustando footer

--- my-app/src/components/Footer/index.jsx
+++ my-app/src/components/Footer/index.jsx
@@ -6,12 +6,9 @@ export default function Footer() {
 
       <div className={style.footerContainerText}>
         <p className={style.footerText}>
-          Este site NÃO é do Facebook:. Este site não faz parte do site do Facebook ou do
+          Este site NÃO é do Facebook: Este site não faz parte do site do Facebook ou do
           Facebook Inc. Além disso, este site NÃO é endossado pelo Facebook de nenhuma maneira.
           FACEBOOK é uma marca comercial independente da FACEBOOK, Inc.
-        </p>
-
-        <p className={style.footerText}>
           Nós NÃO compartilharemos seu endereço de e-mail com ninguém. Você pode cancelar sua
           inscrição a qualquer momento. Ao se cadastrar você está aceitando receber comunicações
           informativas e promocionais.
